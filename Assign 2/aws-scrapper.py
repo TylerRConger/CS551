fe5d524dd1d888ccc31ccb0c3644849dc81426a0
unknown <TylerConger1998@gmail.com>
2023-11-07 23:28:28 -0600

    Updates

--- Assign 2/aws-scrapper.py
+++ Assign 2/aws-scrapper.py
@@ -7,7 +7,8 @@ if __name__ == "__main__":
     # There are so few books this man published here is all of them 
     urlList = [
        "https://www.amazon.com/Algorithm-Design-Manual-Computer-Science/dp/3030542556/ref=sr_1_2?crid=SEBU0DFZSAD&keywords=steven+s.+skiena&qid=1699371429&sprefix=steven+s.+sk%2Caps%2C86&sr=8-2&ufe=app_do%3Aamzn1.fos.18ed3cb5-28d5-4975-8bc7-93deae8f9840",
-
+        "https://www.amazon.com/Programming-Challenges-Contest-Training-Computer/dp/0387001638/ref=sr_1_2?keywords=steven+s.+skiena&qid=1699414063&sr=8-2&ufe=app_do%3Aamzn1.fos.18ed3cb5-28d5-4975-8bc7-93deae8f9840",
+        
     ]
     HEADERS = {
   'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36',
